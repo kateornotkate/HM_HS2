d3496ae557c8690af06eb55b11a23d4e7dab368b
Ekaterina Shadrintseva <kateornotkate@gmail.com>
2022-08-22 13:17:13 +0500

    Создание коллекции в конструкторе класса

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -5,13 +5,14 @@ import java.util.Set;
 public class WordsChecker {
 
     private String text;
+    private Set<String> set;
 
     public WordsChecker(String text) {
         this.text = text;
+        this.set = new HashSet<>(List.of(text.toLowerCase().split("\\P{Alpha}+")));
     }
 
     public boolean hasWord(String word) {
-        Set<String> set = new HashSet<>(List.of(text.toLowerCase().split("\\P{Alpha}+")));
         if (set.contains(word.toLowerCase())) {
             return true;
         } else {
